7b961436c181c9f5561e62384b1038809ed7c9db
Florian Charlot <florian.charlot53@gmail.com>
2023-04-27 08:53:29 +0200

    Initial commit

---
+++ pages/competences.php
@@ -0,0 +1,59 @@
+<div>
+    <ul>
+        <li>
+            <h3>Gérer le patrimoine informatique</h3>
+            <ul>
+                <li>
+                    <h4>Recenser et identifier les ressources numériques</h4>
+                    <a href="?page=1" class="btn btn-primary">Page 1</a>
+                </li>
+                <li>Exploiter des référentiels, normes et standards adoptés par le prestataire informatique</li>
+                <li>Mettre en place et vérifier les niveaux d’habilitation associés à un service</li>
+                <li>Vérifier les conditions de la continuité d’un service informatique</li>
+                <li>Gérer des sauvegardes</li>
+                <li>Vérifier le respect des règles d’utilisation des ressources numériques</li>
+            </ul>
+        </li>
+        <li>
+            <h3>Répondre aux incidents et aux demandes d’assistance et d’évolution</h3>
+            <ul>
+                <li>Collecter, suivre et orienter des demandes</li>
+                <li>Traiter des demandes concernant les services réseau et système, applicatif</li>
+                <li>Traiter des demandes concernant les applications</li>
+            </ul>
+        </li>
+        <li>
+            <h3>Développer la présence en ligne de l’organisation</h3>
+            <ul>
+                <li>Participer à la valorisation de l’image de l’organisation sur les médias numériques en tenant compte du cadre juridique et des enjeux économiques</li>
+                <li>Référencer les services en ligne de l’organisation et mesurer leur visibilité</li>
+                <li>Participer à l’évolution d’un site Web exploitant les données de l’organisation</li>
+            </ul>
+        </li>
+        <li>
+            <h3>Travailler en mode projet</h3>
+            <ul>
+                <li>Analyser les objectifs et les modalités d’organisation d’un projet</li>
+                <li>Planifier les activités</li>
+                <li>Évaluer les indicateurs de suivi d’un projet et analyser les écarts</li>
+            </ul>
+        </li>
+        <li>
+            <h3>Mettre à disposition des utilisateurs un service informatique</h3>
+            <ul>
+                <li>Réaliser les tests d’intégration et d’acceptation d’un service</li>
+                <li>Déployer un service</li>
+                <li>Accompagner les utilisateurs dans la mise en place d’un service</li>
+            </ul>
+        </li>
+        <li>
+            <h3>Organiser son développement professionnel</h3>
+            <ul>
+                <li>Mettre en place son environnement d’apprentissage personnel</li>
+                <li>Mettre en œuvre des outils et stratégies de veille informationnelle</li>
+                <li>Gérer son identité professionnelle</li>
+                <li>Développer son projet professionnel</li>
+            </ul>
+        </li>
+    </ul>
+</div>
